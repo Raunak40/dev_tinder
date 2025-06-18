fac68c8b5fe897bfdff5f120afe2bf393a6ab66b
Raunak <raunaq.try@gmail.com>
2025-06-18 23:50:25 +0530

    exploring get, post, and delete

--- src/app.js
+++ src/app.js
@@ -2,7 +2,20 @@ const express = require('express');
 
 const app = express();//instance of an express.js application
 
-app.get("/",(req, res) => { //request handler function
+app.use("/user",(req, res) => { //request handler function
+    res.send("HaHaHaHa");
+});
+app.get("/user",(req, res) => { 
+    res.send({firstname: "Raunak", lastname: "Razz"});
+});
+app.post("/user",(req, res) => { 
+    res.send("Data Saved Successfully.");
+});
+app.delete("/user",(req, res) => { 
+    res.send("Data Deleted.");
+});
+
+app.get("/",(req, res) => { 
     res.send("Hello this is dashboard.");
 });
 app.get("/test",(req, res) => { //request handler function
@@ -11,20 +24,14 @@ app.get("/test",(req, res) => { //request handler function
 app.get("/home",(req, res) => { //request handler function
     res.send("home home home.");
 });
-// app.use("/",(req, res) => { //request handler function
-//     res.send("Hello this is dashboard.");
-// });
 // app.use("/test",(req, res) => { //request handler function
 //     res.send("Hello from the server.");
-// });
-// app.use("/home",(req, res) => { //request handler function
-//     res.send("home home home.");
-// });
 
-/*app.use("/"): matches all routes starting with /, including /test, /home, etc.
-app.get("/"): only matches exact GET requests to /.*/
+/*
+=> app.use("/"): matches all routes starting with /, including /test, /home, etc. and will also match all the HTTP method API to /test,/home,etc.
+=> app.get("/"): only matches exact GET requests to / and will only handle GET calls to /
+*/
 
-
-app.listen(3000, () => {
-    console.log("Server is successfully listening on port 3000......");
+app.listen(8080, () => {
+    console.log("Server is successfully listening on port 8080......");
 });
