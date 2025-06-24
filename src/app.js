291afa88577621068447b83d6e1bc9751175bde6
Raunak <raunaq.try@gmail.com>
2025-06-24 11:29:07 +0530

    wildacard_error_handling

--- src/app.js
+++ src/app.js
@@ -1,28 +1,21 @@
 const express = require('express');
-const { adminAuth,userAuth } = require('./Middlewares/auth');
+
 const app = express();//instance of an express.js application
 
-//Handle Auth Middleware for all GET, POST ... requests
-app.use("/admin", adminAuth);
 
-app.post("/user/login", (req,res) => {
-    res.send("Login Successful");
-})
-
-app.get("/user/data", userAuth, (req,res) => {
-    res.send("All user data sent.");
-})
-
-app.get("/admin/getAllData", (req,res) => {
-    res.send("All data sent successfully");
-    console.log("all data sent");
+app.get("/user/data", (req,res) => {
+    //Logic of DB call and get user data
+    
+    throw new Error("hi how are you ");
+    res.send("User Data sent");
 });
 
-app.get("/admin/deleteAllData", (req,res) => {
-    res.send("All data deleted successfully");
-    console.log("all data deleted");
+app.use("/", (err,req,res,next) =>{
+    if(err){
+        //log your error response
+        res.status(500).send("Something went wrong");
+    }
 });
-
 
 app.listen(8080, () => {
     console.log("Server is successfully listening on port 8080......");
