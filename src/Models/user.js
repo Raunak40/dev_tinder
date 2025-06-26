2237464679f7cc32bfd8b03e008605687cdfa665
Raunak <raunaq.try@gmail.com>
2025-06-26 15:15:41 +0530

    schema validations

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,24 +1,56 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 20,
+
     },
     lastName: {
         type: String
     },
     emailID: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [isEmail, 'Please fill a valid email address'],
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18, 
     },
     gender: {
-        type: String
+        type: String,
+        trim: true,
+        validate(value){//only works for new object by default
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        },
     },
+    photoURL: {
+        type: String,
+        default: "https://www.istockphoto.com/illustrations/user-profile",
+    },
+    about: {
+        type: String,
+        default: "This is a default user about",
+    },
+    hobbies: {
+        type: [String],
+    }
+},
+{
+    timestamps: true,
 });
 
 const User = mongoose.model("User", userSchema);//MODEL
--- src/app.js
+++ src/app.js
@@ -4,31 +4,97 @@ const User = require('./Models/user');
 const app = express();//instance of an express.js application
 app.use(express.json());
 
-app.post("/signup",async (req,res) => {
+app.post("/signup", async (req, res) => {
 
     console.log(req.body);
     // Creating a new instance of the User Model
     const user = new User(req.body);
 
-    try{
+    try {
         await user.save();
         res.send("User Added successfully");
-    }catch(err){
-        res.status(400).send("Error saving the user" + err.message);
+    } catch (err) {
+        res.status(400).send("Error saving the user: " + err.message);
     }
 })
 
+//GET user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailID;
+    try {
+        const user = await User.find({ emailID: userEmail });
+        if (user.length == 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await User.find({});
+        res.send(user);
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//Delete a user from the database
+app.delete("/user",async (req,res) =>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }
+    catch (err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//Update data of the user by userID
+// app.patch("/user", async(req,res) => {
+//     const userId = req.body.userId;
+//     const data = req.body;
+//     try{
+//         await User.findByIdAndUpdate(userId,data);
+//         res.send("User updatedd successfully");
+//     }
+//     catch (err){
+//         res.status(400).send("Something went wrong");
+//     }
+// });
+
+//Update data of the user by email
+app.patch("/user", async(req,res) => {
+    const userEmail = req.body.emailID;
+    const data = req.body;
+    try{
+        await User.findOneAndUpdate(
+            {emailID: userEmail},
+            {$set: data},
+            {runValidators: true},
+        );
+        res.send("User updatedd successfully");
+    }
+    catch (err){
+        res.status(400).send("UPDATE FAILED: " + err.message);
+    }
+})
 
 connDB()
-        .then(() => {
-            console.log("Database connection established....");
-            app.listen(8080, () => {
-                console.log("Server is successfully listening on port 8080......");
-            });
-        })
-        .catch((err) => {
-            console.log("Database cannot be connected!!");
-        })
+    .then(() => {
+        console.log("Database connection established....");
+        app.listen(8080, () => {
+            console.log("Server is successfully listening on port 8080......");
+        });
+    })
+    .catch((err) => {
+        console.log("Database cannot be connected!!");
+    })
 
 
 
