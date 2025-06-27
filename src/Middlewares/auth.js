a5d45672cbcfafc789ac31bb8d423dda60069cd2
Raunak <raunaq.try@gmail.com>
2025-06-27 16:26:57 +0530

    added userAuth & used JWT for token creation and expiration

--- src/Middlewares/auth.js
+++ src/Middlewares/auth.js
@@ -1,29 +1,24 @@
-const adminAuth = (req,res,next) => {
-    console.log("Admin authorization is being checked");
-    const token = "xyz";
-    const isAdminAuthorised = token == "xyza";
-    if(!isAdminAuthorised){
-        res.status(401).send("Unauthorised Access");
-        console.log("Unauthorised Access");
-    }
-    else{
-        next();
-    }
-};
+const jwt = require("jsonwebtoken");
+const User = require("../Models/user");
 
-const userAuth = (req,res,next) => {
-    console.log("User authorization is being checked");
-    const token = "abc";
-    const isUserAuthorised = token == "abccd";
-    if(!isUserAuthorised){
-        res.status(401).send("Unauthorised Access");
-        console.log("Unauthorised Access");
-    }
-    else{
+const userAuth = async (req,res,next) => {
+    try{
+        const {token} = req.cookies;
+        if(!token){
+            throw new Error("Invalid Token");
+        }
+        const decodedMsg = await jwt.verify(token,"DEV@tinder007");
+        const { _id } = decodedMsg;
+        const user = await User.findById(_id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        req.user = user;
         next();
+    }catch(err){
+        res.status(400).send("ERROR: " + err.message);
     }
 };
 module.exports = {
-    adminAuth,
     userAuth,
 };
--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cookieParser  = require("cookie-parser");
 const connDB = require('./config/database');
 const User = require('./Models/user');
 const { validateSignUpData } = require('./utils/validation');
 const bcrypt = require('bcrypt');
+const { userAuth } = require('./Middlewares/auth');
+const jwt = require('jsonwebtoken');
 const app = express();//instance of an express.js application
+
 app.use(express.json());//Returns middleware that only parses json
+app.use(cookieParser());
 
 //POST new users
 app.post("/signup", async (req, res) => {
@@ -34,6 +39,10 @@ app.post("/login", async (req, res) => {
         }
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (isPasswordValid) {
+            // Create a JWT Token
+            const token = await jwt.sign({_id: user._id}, "DEV@tinder007", {expiresIn: "7d"});
+
+            res.cookie("token",token, {expires: new Date(Date.now() + 1 * 3600000)});
             res.send("Logged in Successfully");
         }
         else {
@@ -41,6 +50,26 @@ app.post("/login", async (req, res) => {
         }
     } catch (err) {
         res.status(400).send("ERROR: " + err.message);
+    }
+})
+
+//GET user profile
+app.get("/profile", userAuth, async(req,res) => {
+    try{
+        user = req.user;
+        res.send(user);
+    }catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+app.post("/sendConnReq",userAuth, async (req,res) => {
+    try{
+        console.log("Connection request sent successfully");
+        user = req.user;
+        res.send(user.firstName + " Sent connection request");
+    }catch(err){
+        res.status(400).send("ERROR: "+ err.message);
     }
 })
 
